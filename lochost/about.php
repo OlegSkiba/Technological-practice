6ef45914c9e7bbb6c5b8d8ae0b9a1a52c3823af0
Oleg Skiba <107154680+OlegSkiba@users.noreply.github.com>
2022-10-14 23:18:26 +0300

    Add files via upload

---
+++ lochost/about.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <title>Контактна форма</title>
+</head>
+<body>
+
+<?php require "blocks/header.php" ?>
+
+<div class="container mt-5">
+<h3>Контактна форма</h3>
+<form action="check.php" method="post">
+    <input type="email" name="email" placeholder="Введіть Email" class="form-control"><br>
+    <textarea name="message" class="form-control" placeholder="Введіть ваше повідомлення"></textarea><br>
+    <button type="submit" name="sent" class="btn btn-success">Відправити</button>
+</form>
+</div>
+
+<?php require "blocks/footer.php" ?>
+
+</body>
+</html>
